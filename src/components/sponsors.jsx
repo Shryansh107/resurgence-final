a440a959f311f104604aaf95d7d5ecaeedbc4d8b
cbof <cbof16@gmail.com>
2023-10-12 21:32:33 +0530

    theme changed

---
+++ src/components/sponsors.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const sponsors = () => {
+  return (
+    <>
+        <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12  text-center md:justify-between">
+
+        </div>
+    </>
+  )
+}
+
+export default sponsors
